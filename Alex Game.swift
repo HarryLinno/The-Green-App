cd15c35f152cd8251b27c25463a9f4b5f1dc87e5
Harry Lin <50762650+HarryLinno@users.noreply.github.com>
2019-06-06 17:11:07 +1000

    Update Alex Game.swift

--- Alex Game.swift
+++ Alex Game.swift
@@ -5,8 +5,6 @@
 //  Created by Harry on 6/6/19.
 //  Copyright © 2019 LIN, Harry. All rights reserved.
 //
-//Image - Red bin, green bin, yellow bin
-//BRICK, GRASS, CAN
 
 import UIKit
 
@@ -14,14 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Once the view loads, a random image is selected from the array "items" and placed into the image property of "imgTrashImage".
         imgTrashImage.image = UIImage(named: "\(items.randomElement()!)")
         // Do any additional setup after loading the view.
     }
+    //Outlets
     @IBOutlet weak var imgTrashImage: UIImageView!
-    var score:Int = 0
-    var items:[String] = ["Brick","Can","Grass"]
     @IBOutlet weak var lblScore: UILabel!
+    //The mutable integer variable "score" keeps track of the user's track record; the higher the score, the higher percentage of correct answers.
+    var score:Int = 0
+    //"items" is an array that contains the names of all the images that will be available in the game. For the purpose of this assignment, the number of images has been limited to 3.
+    var items:[String] = ["Brick","Can","Grass"]
+    //Actions
     @IBAction func btnRed(_ sender: Any) {
+        //The current trash item is put into a switch statement. Each case checks to see if the current image is a certain image. If the user's choice of bin is inappropriate for the item currently displayed, their score is deducted by either 1 or 5, depending on how incorrect they are. If their decision is appropriate, the "score" variable is increased by 10. After the text in "lblScore" has been updated with the user's new score, the image is once again randomised. This process also applies to "btnGreen" and "btnYellow".
         switch imgTrashImage.image {
         case UIImage(named: "Brick"):
             score += 10
@@ -38,6 +42,7 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func btnGreen(_ sender: Any) {
+        //Refer to the description for "btnRed"
         switch imgTrashImage.image {
         case UIImage(named: "Brick"):
             score -= 1
@@ -54,6 +59,7 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func btnYellow(_ sender: Any) {
+        //Refer to the description for "btnRed"
         switch imgTrashImage.image {
         case UIImage(named: "Brick"):
             score -= 1
